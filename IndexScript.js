229a3f0846fc80c588a9f5ab91cffa6dfb2d8a13
Evan-Debug <75247764+Evan-Debug@users.noreply.github.com>
2021-01-09 17:00:43 +1100

    Commit Number: 2

--- IndexScript.js
+++ IndexScript.js
@@ -1,30 +1,6 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
-
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
-})
-
-function checkPswd() {
-        var confirmPassword = "yotsuba nakano";
-        var password = document.getElementById("pswd").value;
-        if (password == confirmPassword) {
-             window.location="hidden.html";
-        }
-        else{
-            alert("Password is Incorrect");
-        }
+function HomeLink(){
+	window.location.href = "index.html";
 }
-function HomePage() {
- window.location="index.html";
+function Link2(){
+	window.location.href = "Link1.html";
 }
